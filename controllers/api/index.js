d0fbf8c958b35c166dcbfb7855a5605e93de1d3b
smithm4949 <smithm4949@gmail.com>
2023-02-23 20:44:30 -0500

    adding eventRoutes to api index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@
 const router = require('express').Router();
 const ownerRoutes = require('./owner-routes');
 const dogRoutes = require('./dog-routes');
+const eventRoutes = require('./event-routes');
 
 router.use('/owners', ownerRoutes);
 router.use('/dogs', dogRoutes);
+router.use('/events', eventRoutes);
 
 module.exports = router;
