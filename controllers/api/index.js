9d59a1e6f3e0ea701c9e089c7a53f95227a915ad
smithm4949 <smithm4949@gmail.com>
2023-02-23 19:06:29 -0500

    added basic CRUD routes for dog model

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 // Create the different routes under the /api route
 const router = require('express').Router();
 const owner = require('./owner-routes');
+const dogRoutes = require('./dog-routes');
 
 router.use('/owner', owner);
+router.use('/dogs', dogRoutes);
 
 module.exports = router;
