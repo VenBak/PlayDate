fcaa568b27d27dcb1d187e72ef4c812153450ba3
smithm4949 <smithm4949@gmail.com>
2023-02-23 20:23:14 -0500

    models/index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 // Import all of the models
 const Owner = require('./Owner');
 const Dog = require('./Dog');
+const Event = require('./Event');
 
 Owner.hasMany(Dog, {
     foreignKey: 'owner_id',
@@ -8,5 +9,25 @@ Owner.hasMany(Dog, {
 });
 
 Dog.belongsTo(Owner);
+
+// Each event was created by and belongs to only one user (owner)
+Event.belongsTo(Owner);
+
+// Owners can create many events
+Owner.hasMany(Event, {
+  foreignKey: 'owner_id'
+});
+
+// Events belong to many owners as people attending the event
+Event.belongsToMany(Owner, {
+  as: 'attendees',
+  foreignKey: 'attendee_id',
+  through: 'event_attendees'
+});
+
+// Owners can attend many events, regardless of who created the event
+Event.belongsToMany(Owner, {
+  through: 'event_attendees'
+});
 
 module.exports = { Owner, Dog }
