98ba8e1043ffe1f4d89cf4804981e01e58beeec7
Claire <lee.claire156@gmail.com>
2023-02-27 18:31:41 -0500

    Adds homepage, login, and sign up pages

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3009;
@@ -11,7 +14,11 @@ app.use(express.urlencoded({ extended: true }));
 // turn on routes
 app.use(routes);
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
