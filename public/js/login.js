b493c7ae39622efaaf1f5198a75364dfc5d4bcf7
MEwing <masandraewing@gmail.com>
2023-02-26 21:32:06 -0500

    Changed email to username

--- public/js/login.js
+++ public/js/login.js
@@ -2,14 +2,14 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
 
     // Obtain value from the login form
-    const email = document.querySelector('#email-login').value.trim(); // TO DO - get id used in handlebar for this one
-    const password = document.querySelector('#password-login').value.trim(); // TO DO - get id used in handlebar for this one
+    const username = document.querySelector('#username-login').value.trim(); 
+    const password = document.querySelector('#password-login').value.trim(); 
 
-    if (email && password) {
+    if (username && password) {
         // Send a POST request to the API endpoint
         const response = await fetch('/api/users/login', { // TO DO - get the name of the route used for logging in
             method: 'POST',
-            body: JSON.stringify({ email, password}),
+            body: JSON.stringify({ username, password}),
             headers: { 'Content-Type': 'application/json' },
         });
         if (response.ok) {
