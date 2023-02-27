21e129f0ef0633510b4af7dc3eab42d660a6f466
MEwing <masandraewing@gmail.com>
2023-02-26 21:28:47 -0500

    Updated signup js to match the Owner and Dog table column titles. Nested the post for dog table within the post for owner table

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,30 +1,60 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
   
+    // OWNER Model columns for the intake form - 
+    // we are not initially asking for picture 
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-    const firstName = document.querySelector('#firstName-signup').value.trim();
-    const lastName = document.querySelector('#lastName-signup').value.trim();
+    const first_name = document.querySelector('#first_name-signup').value.trim();
+    const last_name = document.querySelector('#last_name-signup').value.trim();
     const gender = document.querySelector('#gender-signup').value.trim();
-    const locationZip = document.querySelector('#locationZip-signup').value.trim();
+    const location_zip = document.querySelector('#location_zip-signup').value.trim();
     const description = document.querySelector('#description-signup').value.trim();
   
-    if (username && password && firstName && lastName && gender && locationZip && description) {
+    // POST to Owner table
+    if (username && password && first_name && last_name && gender && location_zip && description) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ username, password, firstName, lastName, gender, locationZip, description }),
+        body: JSON.stringify({ username, password, first_name, last_name, gender, location_zip, description }),
         headers: { 'Content-Type': 'application/json' },
       });
-  
+
+      // IF response is successful, then call posting to DOG table
       if (response.ok) {
-        document.location.replace('/profile');
+        postDog();
       } else {
         alert(response.statusText);
       }
+    
+          const postDog = async (event) => {
+            event.preventDefault();
+
+            // DOG Model columns for the intake form - 
+            // we are not initially asking for picture 
+            const name = document.querySelector('#dog-name-signup').value.trim();
+            const age = document.querySelector('#dog-name-signup').value.trim();
+            const breed = document.querySelector('#dog-name-signup').value.trim();
+            const gender = document.querySelector('#dog-name-signup').value.trim();
+
+              // POST to Dog table
+              if (name && age && breed && dogGender) {
+                const response = await fetch('/api/dogs', {
+                  method: 'POST',
+                  body: JSON.stringify({ name, age, breed, gender }),
+                  headers: { 'Content-Type': 'application/json' },
+                });
+
+                // IF response is successful, then go to the profile
+                if (response.ok) {
+                  document.location.replace('/profile');
+                } else {
+                  alert(response.statusText);
+                }
+              }
+          }
     }
-  };
+};
   
   document
     .querySelector('.signup-form')
     .addEventListener('submit', signupFormHandler);
-  
