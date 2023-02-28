239f1448704cf35c6913bd69ccbdd99564d3ea40
smithm4949 <smithm4949@gmail.com>
2023-02-27 19:19:14 -0500

    adding api routes for login/logout

--- server.js
+++ server.js
@@ -4,10 +4,28 @@ const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const path = require('path');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3009;
 
+const sess = {
+  secret: 'andre claire masandra myles',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
