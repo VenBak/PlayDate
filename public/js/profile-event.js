6513dc8beb2e30f268e01c3e3929b40489652596
MEwing <masandraewing@gmail.com>
2023-02-28 16:21:03 -0500

    Commented out Delete event for now since waiting on render of owner's events on the profile page.

--- public/js/profile-event.js
+++ public/js/profile-event.js
@@ -1,7 +1,7 @@
 // Show the dog form and reset the fields
 function showEventForm() {
   const eventForm = document.querySelector('form');
-  eventForm.reset();
+    eventForm.reset();
     document.querySelector('.eventForm').style.display = 'block';
     document.querySelector('#submitEvent-btn').style.display = 'block';
   }
@@ -42,23 +42,23 @@ const addEventFormHandler = async (event) => {
   document.querySelector('#submitEvent-btn').addEventListener('click', addEventFormHandler);
 
 
-  // DELETE an event
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
+  // // DELETE an event
+  // const delButtonHandler = async (event) => {
+  //   if (event.target.hasAttribute('data-id')) {
+  //     const id = event.target.getAttribute('data-id');
   
-      const response = await fetch(`/api/events/${id}`, {
-        method: 'DELETE',
-      });
+  //     const response = await fetch(`/api/events/${id}`, {
+  //       method: 'DELETE',
+  //     });
   
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to delete event profile');
-      }
-    }
-  };
+  //     if (response.ok) {
+  //       document.location.replace('/profile');
+  //     } else {
+  //       alert('Failed to delete event profile');
+  //     }
+  //   }
+  // };
   
-  document
-    .querySelector('.event-list').addEventListener('click', delButtonHandler);
+  // document
+  //   .querySelector('.event-list').addEventListener('click', delButtonHandler);
   
