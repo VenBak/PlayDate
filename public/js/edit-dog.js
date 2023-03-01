d8ba6a5a74d9eda0275b2b049b5647491ff986ea
MEwing <masandraewing@gmail.com>
2023-02-28 22:22:41 -0500

    Edit dog feature working

--- public/js/edit-dog.js
+++ public/js/edit-dog.js
@@ -1,17 +1,17 @@
-// EDIT a dog
+    // EDIT a dog
 
-function showEditDogForm() {
-    const editDogForm = document.querySelector('form');
-      editDogForm.reset();
+    function showEditDogForm() {
       document.querySelector('.edit-dog-form').style.display = 'block';
       document.querySelector('#submitEdit-btn').style.display = 'block';
     }
   
-  // Hide the dog form after the form has been submitted
-  function hideEditDogForm() {
+
+    // Hide the dog form after the form has been submitted
+    function hideEditDogForm() {
     document.querySelector('.edit-dog-form').style.display = 'none';
     document.querySelector('#submitEdit-btn').style.display = 'none';
-  }
+    }
+  
   
     const editDogFormHandler = async (event) => {
       event.preventDefault();
@@ -20,21 +20,27 @@ function showEditDogForm() {
       const age = document.querySelector('#editdog-age').value.trim();
       const breed = document.querySelector('#editdog-breed').value.trim();
       const gender = document.querySelector('#editdog-gender').value.trim();
-  
-      if (event.target.hasAttribute('data-id')) {
-        const id = event.target.getAttribute('data-id');
+
+      const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length -1
+      ];
+
+      console.log(id);
   
         const response = await fetch(`/api/dogs/${id}`, {
           method: 'PUT',
           body: JSON.stringify({ name, age, breed, gender }),
           headers: { 'Content-Type': 'application/json' },
-      });
+        });
+      
         // IF response is successful, then reload
         if (response.ok) {
           document.location.reload();
         } else {
           alert(response.statusText);
         }
-      }
+      
+    
     };
+    
     document.querySelector('#submitEdit-btn').addEventListener('click', editDogFormHandler);
