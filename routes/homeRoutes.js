d070627f0d0540fa0dc0045768efa3e8251775eb
smithm4949 <smithm4949@gmail.com>
2023-03-01 19:17:26 -0500

    updating events render to get events user is hosting

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { Owner, Dog, Event } = require('../models');
+const eventController = require('../routes/controllers/eventController');
 
 router.get('/', async (req, res) => {
     res.render('homepage', {
@@ -23,11 +25,23 @@ router.get('/signup', (req, res) => {
     });
 });
 
-router.get('/events', (req, res) => {
-    //Renders signup handlebars template on the signup page
-    res.render('homepage', {
-        logged_in: req.session.logged_in
-    });
+router.get('/events', async (req, res) => {
+    // Send the rendered Handlebars.js template back as the response
+    try {
+        const eventData = await Event.findAll({
+            where: {host_id: req.session.id}
+        });
+        //  res.status(200).json(dogData); //For testing only
+        const events = dogData.get({ plain: true });
+
+        res.render('homepage', {
+            events,
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
+
 
 module.exports = router;
