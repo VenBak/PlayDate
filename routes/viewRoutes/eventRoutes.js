2a3ddb3b8f9eb627b3ff85fb412dbbdda8dd8678
smithm4949 <smithm4949@gmail.com>
2023-03-02 12:15:25 -0500

    adding /events/:id render

--- routes/viewRoutes/eventRoutes.js
+++ routes/viewRoutes/eventRoutes.js
@@ -4,10 +4,20 @@ const eventController = require('../controllers/eventController');
 router.get('/', (req, res) => {
   eventController.getAll(req, res)
   .then(events => {
-    plainEvents = events.map(event => event.get({ plain: true }))
-    console.log(plainEvents)
-    console.log(req.session)
+    plainEvents = events.map(event => event.get({ plain: true }));
     res.status(200).render('events', {events: plainEvents, logged_in: req.session.logged_in});
+  })
+  .catch(err => {
+      res.redirect('../');
+      console.log(err);
+  })
+});
+
+router.get('/:id', (req, res) => {
+  eventController.getOne(req, res)
+  .then(event => {
+    plainEvent = event.get({ plain: true });
+    res.status(200).render('eventsprofile', {...event, logged_in: req.session.logged_in});
   })
   .catch(err => {
       res.redirect('../');
