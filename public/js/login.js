4e67199b24775ad706a9fff668aecd6c5909b830
Claire Lee <lee.claire156@gmail.com>
2023-03-03 11:56:15 -0500

    Adds no username/password login validation

--- public/js/login.js
+++ public/js/login.js
@@ -21,6 +21,40 @@ const loginFormHandler = async (event) => {
     }
 };
 
+async function noInputValidation(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+    const passwordlength = password.length
+
+    const usernameError = document.querySelector('.username-error-msg')
+    const passwordError = document.querySelector('.password-error-msg')
+
+    if (!username) {
+        usernameError.classList.remove("d-none")
+        passwordError.classList.remove("d-none")
+    } else {
+        usernameError.classList.add("d-none")
+        passwordError.classList.add("d-none")
+    }
+
+    if (!password || passwordlength < 8 || passwordlength > 16) {
+        usernameError.classList.remove("d-none")
+        passwordError.classList.remove("d-none")
+    } else {
+        usernameError.classList.add("d-none")
+        passwordError.classList.add("d-none")
+    }
+
+    if (!username || !password || passwordlength < 8 || passwordlength > 16) {
+        return
+    }
+}
+
+
+
+
 document
     .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
+    .addEventListener('submit', noInputValidation, loginFormHandler);
