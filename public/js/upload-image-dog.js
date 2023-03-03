2d75b6f0f1411fa54cbc337ae0978486c12c0e74
Claire <lee.claire156@gmail.com>
2023-03-02 21:40:44 -0500

    Clean up

--- public/js/upload-image-dog.js
+++ public/js/upload-image-dog.js
@@ -1,30 +1,30 @@
 // TO BE DELETED
 
-var myWidget = cloudinary.createUploadWidget({
-    cloudName: 'dlkk2oyhp',
-    uploadPreset: 'playedate_preset'
-}, (error, result) => {
-    if (!error && result && result.event === "success") {
-        // console.log('Done! Here is the image info: ', result.info);
-        var pic_hyperlink = result.info.secure_url
+// var myWidget = cloudinary.createUploadWidget({
+//     cloudName: 'dlkk2oyhp',
+//     uploadPreset: 'playedate_preset'
+// }, (error, result) => {
+//     if (!error && result && result.event === "success") {
+//         // console.log('Done! Here is the image info: ', result.info);
+//         var pic_hyperlink = result.info.secure_url
 
-        // Changes preview image to the uploaded image
-        document.getElementById("preview-img").src = pic_hyperlink;
-    }
-}
-)
+//         // Changes preview image to the uploaded image
+//         document.getElementById("preview-img").src = pic_hyperlink;
+//     }
+// }
+// )
 
-document.getElementById("upload_widget").addEventListener("click", function () {
-    myWidget.open();
-}, false);
+// document.getElementById("upload_widget").addEventListener("click", function () {
+//     myWidget.open();
+// }, false);
 
-const updateDogPhoto = async (event) => {
-    event.preventDefault();
-
-    
+// const updateDogPhoto = async (event) => {
+//     event.preventDefault();
 
     
-}
+
+    
+// }
 
 // document.querySelector('#upload-img-btn').addEventListener('click', updateDogPhoto);
 
