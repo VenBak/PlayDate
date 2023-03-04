6d2b29c5d60b08eb1479ac81b05d8235b270c4b7
MEwing <masandraewing@gmail.com>
2023-03-03 19:26:51 -0500

    styling

--- public/js/codeHolder.js
+++ public/js/codeHolder.js
@@ -9,3 +9,26 @@
 
 // placeholder="{{start_date}}" onfocus="(this.type='date')"
 // placeholder="{{end_date}}" onfocus="(this.type='date')"
+
+// // Navigation function tied to profile.handlebars
+// // tied to the select tag via onchange
+
+// function navFormHandler(val) {
+  
+//     if (val == 'userNav') {
+//         document.querySelector('#owner-section').style.display = 'block';
+//         document.querySelector('#dog-section').style.display = 'none';
+//         document.querySelector('#events-section').style.display = 'none';
+//     }
+//     else if (val == 'dogsNav') {
+//         document.querySelector('#owner-section').style.display = 'none';
+//         document.querySelector('#dog-section').style.display = 'block';
+//         document.querySelector('#events-section').style.display = 'none';
+//     } else {
+//         document.querySelector('#owner-section').style.display = 'none';
+//         document.querySelector('#dog-section').style.display = 'none';
+//         document.querySelector('#events-section').style.display = 'block'; }
+// };
+
+// {{!-- Allows users navigate the profile page
+//     <script src="/js/profile-navigation.js"></script> --}}
