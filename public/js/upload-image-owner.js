9cbc82558ee8263f7a0993a423b884483321fa4b
Claire Lee <lee.claire156@gmail.com>
2023-03-05 13:43:59 -0500

    Removes upload images js from public/js

--- public/js/upload-image-owner.js
+++
@@ -1,7 +0,0 @@
-const uploadBtn = document.querySelector(".upload-owner-btn");
-uploadBtn.value = "Upload";
-
-uploadBtn.addEventListener("submit", (event) => {
-    event.preventDefault();
-    uploadBtn.value = "Upload !"
-})
